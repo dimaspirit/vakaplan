1c56d3505bedf26065dbe720b52df8a4a62f89ca
Dmytro Lobanov <dspwww@gmail.com>
2025-06-30 12:19:36 +0100

    feat: Add ui/tabs component

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,10 +1,19 @@
 import { Button } from "@/components/ui/button"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 
 function AuthPage() {
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
+        <Tabs defaultValue="account" className="w-[400px]">
+          <TabsList>
+            <TabsTrigger value="account">Account</TabsTrigger>
+            <TabsTrigger value="password">Password</TabsTrigger>
+          </TabsList>
+          <TabsContent value="account">Make changes to your account here.</TabsContent>
+          <TabsContent value="password">Change your password here.</TabsContent>
+        </Tabs>
         <Button>Continue</Button>
       </div>
     </div>
