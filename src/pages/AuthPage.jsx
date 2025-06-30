3b43d53e37c752f6e46a49f21609474d1676c9f6
Dmytro Lobanov <dspwww@gmail.com>
2025-06-30 12:26:57 +0100

    feat: Add form, input, label comp. from shadcn

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -6,15 +6,7 @@ function AuthPage() {
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <Tabs defaultValue="account" className="w-[400px]">
-          <TabsList>
-            <TabsTrigger value="account">Account</TabsTrigger>
-            <TabsTrigger value="password">Password</TabsTrigger>
-          </TabsList>
-          <TabsContent value="account">Make changes to your account here.</TabsContent>
-          <TabsContent value="password">Change your password here.</TabsContent>
-        </Tabs>
-        <Button>Continue</Button>
+        <LoginForm />
       </div>
     </div>
   )
