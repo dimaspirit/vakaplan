3a5db0d5e3c04e4614d07bd0f65553450644aebd
Dmytro Lobanov <dspwww@gmail.com>
2025-06-30 14:32:40 +0100

    feat(Auth): Add signup func. with validation

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,4 +1,4 @@
-
+import { useNavigate } from "react-router";
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -10,8 +10,51 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+
+import { firebaseSignUp } from "@/services/auth"; 
+
+const formSchema = z.object({
+  email: z.string()
+    .min(1, { message: "This field has to be filled." })
+    .email("This is not a valid email."),
+  password: z.string()
+    .min(6, { message: "Password must be at least 6 characters long." })
+    .max(24, { message: "Password must be at most 24 characters long." }),  
+})
+
 
 export function SignupForm() {
+  const navigate = useNavigate();
+  
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+  const onSubmit = (data) => {
+    firebaseSignUp(data)
+      .then(() => {
+        navigate("/");
+      }).catch(error => {
+        console.log(error);
+      });
+  }
+
   return (
     <div className="flex flex-col gap-6">
       <Card>
@@ -20,30 +63,54 @@ export function SignupForm() {
           <CardDescription>Your journey continues here</CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
-            <div className="grid gap-6">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
               <div className="grid gap-6">
-                <div className="grid gap-3">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
-                    required
-                  />
-                </div>
-                <div className="grid gap-3">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
+                <div className="grid gap-6">
+                  <div className="grid gap-3">
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <Input placeholder="example@mail.com" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                          <FormDescription>
+                            Enter your email address to continue.
+                          </FormDescription>
+                        </FormItem>
+                      )}
+                    />
                   </div>
-                  <Input id="password" type="password" required />
+                  <div className="grid gap-3">
+                    <FormField
+                      control={form.control}
+                      type="password"
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Password</FormLabel>
+                          <FormControl>
+                            <Input type="password" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                          <FormDescription>
+                            Be sure to use a strong password. Use min 6 characters and max 24 characters.
+                          </FormDescription>
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <Button type="submit" className="w-full">
+                    Signup
+                  </Button>
                 </div>
-                <Button type="submit" className="w-full">
-                  Signup
-                </Button>
               </div>
-            </div>
-          </form>
+            </form>
+          </Form>
         </CardContent>
       </Card>
     </div>
