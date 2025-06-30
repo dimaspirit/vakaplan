521768bcd019f6d62fb5e968b40c30652855b0ec
Dmytro Lobanov <dspwww@gmail.com>
2025-06-30 12:44:57 +0100

    feat: Add Card component from shadcn/ui

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -6,7 +6,7 @@ function AuthPage() {
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginForm />
+        <p>Login</p>
       </div>
     </div>
   )
