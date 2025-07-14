6f3d92585c2a1a400901e5ac0ee6aee37380fdc7
Dmytro Lobanov <dspwww@gmail.com>
2025-07-14 17:13:25 +0200

    feat: Add signout in Setting page (#5)

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,8 +1,42 @@
+import { useState } from "react";
+import { Button } from "@/components/ui/button"
+import { Loader2Icon, LogOutIcon } from "lucide-react"
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import useAuthStore from "../store/authStore";
 
 function SettingsPage() {
+  const [isLogoutLoading, setIsLogoutLoading] = useState(false);
+  const signout = useAuthStore(state => state.signout);
+
+  const handleLogout = async() => {
+    setIsLogoutLoading(true);
+    signout();
+  }
+
   return (
     <>
-      <p>SettingsPage</p>
+      <h1 className="scroll-m-20 text-2xl font-bold tracking-tight text-balance">
+        Settings
+      </h1>
+
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Danger Zone</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Button variant="secondary" className="w-full" disabled={isLogoutLoading} onClick={handleLogout} >
+            {!isLogoutLoading && <LogOutIcon />}
+            {isLogoutLoading && <Loader2Icon className="animate-spin" />}
+            Log out from 
+          </Button>
+        </CardContent>
+      </Card>
     </>
   )
 }
